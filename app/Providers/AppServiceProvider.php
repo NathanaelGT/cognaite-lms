1cb38c95782563de253ea32a7949f60f7517373a
NathanaelGT <gordontanu2004@gmail.com>
2025-03-24 20:44:41 +0700

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        LanguageSwitch::configureUsing(static function (LanguageSwitch $switch) {
+            $switch->locales(['id', 'en']);
+        });
+    }
+}
