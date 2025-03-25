1361310f238929a66aff25493cb2286f444e001d
NathanaelGT <gordontanu2004@gmail.com>
2025-03-25 22:36:10 +0700

    chore: Create cohort panel

---
+++ app/Providers/Filament/CohortPanelProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Enums\ThemeMode;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class CohortPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('cohort')
+            ->path('')
+            ->spa()
+            ->login()
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->defaultThemeMode(ThemeMode::Light)
+            ->favicon(asset('img/logo.svg'))
+            ->brandName('Cognaite LMS')
+            ->discoverResources(in: app_path('Filament/Cohort/Resources'), for: 'App\\Filament\\Cohort\\Resources')
+            ->discoverPages(in: app_path('Filament/Cohort/Pages'), for: 'App\\Filament\\Cohort\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Cohort/Widgets'), for: 'App\\Filament\\Cohort\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                Widgets\FilamentInfoWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
