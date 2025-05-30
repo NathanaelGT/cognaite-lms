0312379d9e47527e64a2ae8bee0b21e967ca60a7
Michael <mick.christiano@gmail.com>
2025-05-31 01:17:28 +0700

    fix: migration error

--- database/migrations/2025_05_31_003433_add_submission_file_to_posts_table.php
+++ database/migrations/2025_05_31_003433_add_submission_file_to_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('submission_file')->nullable()->after('type');
+            $table->string('submission_file')->nullable();
         });
     }
 
